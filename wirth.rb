bb656bbeafc9d8fa1f1e9a6f645a6417d08059be
Pepe Doval <pepellou@gmail.com>
2012-12-12 14:56:26 +0100

    Cache computations
    Tests runtime without cache: 1 min 21s
    Tests runtime with cache: 0.5s

--- wirth.rb
+++ wirth.rb
@@ -1,21 +1,33 @@
 class Fixnum
 
+	@@hash_wirth_strings = Hash.new
+	@@hash_wirth_candidates = Hash.new
+	@@hash_disallowed_strings = Hash.new
+
 	def wirth
+		return @@hash_wirth_strings[self] if @@hash_wirth_strings.has_key?(self)
 		candidates = wirth_candidates
 		disallowed_strings.each do |string|
 			candidates.delete_if { |e| e.include?(string) }
 		end
+		@@hash_wirth_strings[self] = candidates
 		return candidates
 	end
 
 	def disallowed_strings
-		((1..self/2).to_a.map { |n| n.wirth }).flatten.map { |string| string + string }
+		return @@hash_disallowed_strings[self] if @@hash_disallowed_strings.has_key?(self)
+		result = ((1..self/2).to_a.map { |n| n.wirth }).flatten.map { |string| string + string }
+		@@hash_disallowed_strings[self] = result
+		return result
 	end
 
  	def wirth_candidates
+		return @@hash_wirth_candidates[self] if @@hash_wirth_candidates.has_key?(self)
 		alphabet = ['A', 'B', 'C']
 		return alphabet if self == 1
-		(self - 1).wirth_candidates.product(alphabet).map { |candidate, suffix| candidate + suffix }
+		result = (self - 1).wirth_candidates.product(alphabet).map { |candidate, suffix| candidate + suffix }
+		@@hash_wirth_candidates[self] = result
+		return result
 	end
 
 end
