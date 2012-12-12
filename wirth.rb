fafe70f7333ca8d1fe199f60f20f1a2c3f1b8306
Pepe Doval <pepellou@gmail.com>
2012-12-12 14:58:38 +0100

    Refactor

--- wirth.rb
+++ wirth.rb
@@ -1,33 +1,33 @@
 class Fixnum
 
-	@@hash_wirth_strings = Hash.new
-	@@hash_wirth_candidates = Hash.new
-	@@hash_disallowed_strings = Hash.new
+	@@cache_strings = Hash.new
+	@@cache_candidates = Hash.new
+	@@cache_disallowed = Hash.new
 
 	def wirth
-		return @@hash_wirth_strings[self] if @@hash_wirth_strings.has_key?(self)
+		return @@cache_strings[self] if @@cache_strings.has_key?(self)
 		candidates = wirth_candidates
 		disallowed_strings.each do |string|
 			candidates.delete_if { |e| e.include?(string) }
 		end
-		@@hash_wirth_strings[self] = candidates
-		return candidates
+		@@cache_strings[self] = candidates
+		candidates
 	end
 
 	def disallowed_strings
-		return @@hash_disallowed_strings[self] if @@hash_disallowed_strings.has_key?(self)
+		return @@cache_disallowed[self] if @@cache_disallowed.has_key?(self)
 		result = ((1..self/2).to_a.map { |n| n.wirth }).flatten.map { |string| string + string }
-		@@hash_disallowed_strings[self] = result
-		return result
+		@@cache_disallowed[self] = result
+		result
 	end
 
  	def wirth_candidates
-		return @@hash_wirth_candidates[self] if @@hash_wirth_candidates.has_key?(self)
+		return @@cache_candidates[self] if @@cache_candidates.has_key?(self)
 		alphabet = ['A', 'B', 'C']
 		return alphabet if self == 1
 		result = (self - 1).wirth_candidates.product(alphabet).map { |candidate, suffix| candidate + suffix }
-		@@hash_wirth_candidates[self] = result
-		return result
+		@@cache_candidates[self] = result
+		result
 	end
 
 end
